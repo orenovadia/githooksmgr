8934f295e8ca6034b1d4590751e1a30006eda55b
orenovadia <oren@twiggle.com>
2017-06-07 08:46:18 +0300

    install_git_hooks.sh: Removed debug mode

--- scm/operations/install_git_hooks.sh
+++ scm/operations/install_git_hooks.sh
@@ -2,7 +2,7 @@
 # see https://stackoverflow.com/questions/4592838/symbolic-link-to-a-hook-in-git
 
 function debug(){
-true && echo $@
+false && echo $@
 }
 
 ROOT_DIR=`git rev-parse --show-toplevel 2>/dev/null`
@@ -12,6 +12,6 @@ debug "Running install_git_hooks.sh"
 
 for hook in `ls ../../scm/githooks` ;
 do
-    ln -s -f "../../scm/githooks/${hook}" "${hook}"
     debug "Linking ${hook}"
+    ln -s -f "../../scm/githooks/${hook}" "${hook}"
 done
